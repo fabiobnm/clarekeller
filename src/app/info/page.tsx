6bcccd8492f0e6f2478336391b3b0d9ad9d08baa
fabiobnm <ffabiobnm@gmail.com>
2025-11-10 16:26:30 +0100

    Add files via upload

--- src/app/info/page.tsx
+++ src/app/info/page.tsx
@@ -1,13 +1,16 @@
 import Menu from '@/components/Menu/MenuServer';
 import { hygraph } from '@/lib/hygraph';
 import ReactMarkdown from 'react-markdown';
-import { INFO_PAGE_QUERY, type Info } from '@/lib/queries/info';
 import Footer from '@/components/Footer/Footer';
-
-
-
+import {
+  INFO_PAGE_QUERY,
+  type Info,
+  type ExternalLink,
+} from '@/lib/queries/info';
 
 export default async function Page() {
+  // ATTENZIONE: INFO_PAGE_QUERY deve avere gli alias:
+  // linksList: links { ... }  e  linksList2: link2 { ... }
   const { infos } = await hygraph.request<{ infos: Info[] }>(INFO_PAGE_QUERY);
   const info = infos?.[0];
 
@@ -20,53 +23,64 @@ export default async function Page() {
     );
   }
 
+  // TENIAMO LE DUE LISTE SEPARATE
+  const links1 = (info.linksList ?? []).filter(
+    (x): x is ExternalLink => x?.__typename === 'ExternalLink'
+  );
+  const links2 = (info.linksList2 ?? []).filter(
+    (x): x is ExternalLink => x?.__typename === 'ExternalLink'
+  );
+
   return (
     <div>
-        <Menu/>
-      <main className='infoContent slide-in-top' > 
-       
-       <div className='blockInfo'>
-        <div className='textInfo'>
-  <ReactMarkdown>{info.infoText?.markdown ?? ''}</ReactMarkdown>
-</div>
+      <Menu />
+      <main className="infoContent">
+        <div className="blockInfo">
+          <div className="textInfo">
+            <ReactMarkdown>{info.infoText?.markdown ?? ''}</ReactMarkdown>
+          </div>
 
-    <div className='infoLink'>
-      link
-      <div>
-        <br />
-  {info.links?.length ? (
-    <ul>
-      {info.links.map((l) => (
-        <div key={l.id}>
-          <a href={l.link} className='infoLinkVoice' target="_blank" rel="noopener noreferrer">
-            {l.title}
-          </a>
+          <div className="infoLink">
+            <div style={{ marginBottom: 12 }}>link</div>
+            {links1.length > 0 && (
+              <ul>
+                {links1.map((l) => (
+                  <li key={l.id}>
+                    <a
+                      href={l.link}
+                      className="infoLinkVoice"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      {l.title}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            )}
+
+            <div style={{ marginTop: 24, marginBottom: 12 }}>link2</div>
+            {links2.length > 0 && (
+              <ul>
+                {links2.map((l) => (
+                  <li key={l.id}>
+                    <a
+                      href={l.link}
+                      className="infoLinkVoice"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      {l.title}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            )}
+          </div>
         </div>
-      ))}
-    </ul>
-  ) : null}
-</div>
-<br />
-    link2
-      <div>
-        <br />
-  {info.link2?.length ? (
-    <ul>
-      {info.link2.map((l) => (
-        <div key={l.id}>
-          <a href={l.link} className='infoLinkVoice' target="_blank" rel="noopener noreferrer">
-            {l.title}
-          </a>
-        </div>
-      ))}
-    </ul>
-  ) : null}
-</div>
-    </div>
-      </div>
-<Footer/>
+
+        <Footer />
       </main>
-     
     </div>
   );
 }
